c383739200f8f5d08abbc15b5c6eaf079a4801bc
AlbyFer <ferrando2009@gmail.com>
2020-04-02 19:11:59 +0200

    Completed observation matrix building

--- BorsaReader/BorsaReader/Model_builder.py
+++ BorsaReader/BorsaReader/Model_builder.py
@@ -1,13 +1,9 @@
 import pandas as pd
 import pickle as pkl
-import requests
-from bs4 import BeautifulSoup
-import re
-from bs4.diagnose import diagnose
-import time
-import os
+import numpy as np
 
-/Users/albertoferrando/Google\ Drive/Progetti/Investimenti/BorsaReader/Storage/BorsaMilano_dati_aziende.pkl
+
+
 
 
 def read_prices(path_to_folder, ticker):
@@ -35,17 +31,35 @@ def format_financials_prices(financials, ticker, path_to_folder_prices, interest
         print('No price data for company ' + ticker)
         return None
 
+    if financials[ticker] is None:
+        return None
+    else:
+        pass
+
     upper_date = financials[ticker].columns[-1]
     lower_date = financials[ticker].columns[-2]
 
     prices = hist_prices.loc[(hist_prices.index >= lower_date) & (hist_prices.index <= upper_date), 'Close']
     median_price = prices.median()
 
-    result = financials[ticker].loc[interest_ratios, lower_date]
+    result = financials[ticker].reindex(interest_ratios)[lower_date]
     result['Price'] = median_price
     result['Company'] = ticker
 
-    return result
+    result.index = pd.Index([x.replace('%', 'perc') for x in result.index])
+    result.index = pd.Index([x.replace('\xa0', ' ') for x in result.index])
+    result.name = None
+
+
+    if 'Dividends EUR' in result.index:
+        if np.isnan(result['Dividends EUR']):
+            result['Dividends EUR'] = 0
+        else:
+            return result.to_frame().T
+    else:
+        pass
+
+    return result.to_frame().T
 
 
 
---
+++ BorsaReader/create_ObservationMat.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+from BorsaReader.Model_builder import read_financials, format_financials_prices
+
+
+ratios = ["Revenue EUR Mil", "Earnings Per Share EUR", "Dividends EUR", "Shares Mil", "Operating Cash Flow EUR Mil",
+          "Working Capital EUR Mil", "Return on Equity %", "Return on Assets %", "Interest Coverage",
+          "Cap Ex as a % of Sales", "Total Stockholders' Equity", "Days Sales Outstanding", "Payables Period",
+          "Inventory Turnover", "Fixed Assets Turnover", "Current Ratio", "Debt/Equity", "R&D"]
+
+path_to_financials = '/Users/albertoferrando/Google Drive/Progetti/Investimenti/BorsaReader/Storage/BorsaMilano_dati_aziende.pkl'
+path_to_folder_prices = '/Users/albertoferrando/Google Drive/Progetti/Investimenti/BorsaReader/Storage/Prezzi/'
+path_to_save = '/Users/albertoferrando/Google Drive/Progetti/Investimenti/BorsaReader/Storage/Obs_mat.csv'
+
+financials = read_financials(path_to_financials)
+
+data = []
+for ticker in financials.keys():
+    print('Executing company ' + ticker)
+    data.append(format_financials_prices(financials, ticker, path_to_folder_prices, ratios))
+
+obs_mat = pd.concat(data, axis=0)
+obs_mat.set_index('Company', drop=True, inplace=True)
+obs_mat.to_csv(path_to_save)
